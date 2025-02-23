1fe1b6337b6ea113bc7703e4cab8e110be7de0d5
Abhijit <abhijitskushwah@gmail.com>
2025-02-23 20:51:09 +0530

    First Commit

---
+++ src/main/java/com/java_ci_cd/java_spring_boot_ci_cd/JavaSpringBootCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.java_ci_cd.java_spring_boot_ci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java_ci_cd/java_spring_boot_ci_cd/JavaSpringBootCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_ci_cd.java_spring_boot_ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
